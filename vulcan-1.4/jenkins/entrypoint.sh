af0f104e7d76f151d9354407c61426bade16aeef
Durgasuresh Kagitha <durgasuresh.kagitha@oracle.com>
2020-12-04 02:37:59 +0530

    changes to accomadate entrypoiunt and jenkins dockerfile

--- vulcan-1.4/jenkins/entrypoint.sh
+++ vulcan-1.4/jenkins/entrypoint.sh
@@ -3,7 +3,7 @@ set -x
 
 echo $PROJECT_HOME
 
-if [ ! -f $PROJECT_HOME/.gradlew ]; then
+if [ ! -f $PROJECT_HOME/gradlew ]; then
     cd $PROJECT_HOME && mv *gradle* jenkins/
     cd $PROJECT_HOME && gradle init
     cd $PROJECT_HOME && mv jenkins/*gradle* $PROJECT_HOME
