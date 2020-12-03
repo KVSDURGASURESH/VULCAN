0805fd649fd1057da1ce219b0b8971da9b5f0095
Durgasuresh Kagitha <durgasuresh.kagitha@oracle.com>
2020-12-04 01:34:41 +0530

    adding auto increment project level gradle task

--- vulcan-1.4/jenkins/entrypoint.sh
+++ vulcan-1.4/jenkins/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-
 set -x 
 
 echo $PROJECT_HOME
-cd $PROJECT_HOME && mv *gradle* jenkins
-cd $PROJECT_HOME && gradle init
-cd $PROJECT_HOME && mv jenkins/*gradle* .
+
+if [ ! -f $PROJECT_HOME/.gradlew ]; then
+	echo `find $PROJECT_HOME -maxdepth 1 -iname '*gradle*' -exec mv {} jenkins \;`
+	cd $PROJECT_HOME && gradle init
+	echo `find $PROJECT_HOME/jenkins -iname '*gradle*' -exec mv {} $PROJECT_HOME \;`
+fi
